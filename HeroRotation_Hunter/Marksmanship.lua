3166fac69392092198da1307c06474c9c7abb761
AyaProfiles <77168918+AyaProfiles@users.noreply.github.com>
2022-01-22 23:23:19 -0600

    BM - Initial

---
+++ HeroRotation_Hunter/Marksmanship.lua
@@ -0,0 +1,25 @@
+--- ============================ HEADER ============================
+--- ======= LOCALIZE =======
+-- Addon
+local addonName, addonTable = ...
+-- HeroDBC
+local DBC = HeroDBC.DBC
+-- HeroLib
+local HL         = HeroLib
+local Cache      = HeroCache
+local Unit       = HL.Unit
+local Player     = Unit.Player
+local Target     = Unit.Target
+local Pet        = Unit.Pet
+local Spell      = HL.Spell
+local MultiSpell = HL.MultiSpell
+local Item       = HL.Item
+-- HeroRotation
+local HR         = HeroRotation
+
+
+local function Init()
+  HR.Print("Marksmanship Hunter rotation is currently a work in progress.")
+end
+
+HR.SetAPL(254, APL, Init)
---
+++ HeroRotation_Hunter/Survival.lua
@@ -0,0 +1,23 @@
+--- ============================ HEADER ============================
+--- ======= LOCALIZE =======
+-- Addon
+local addonName, addonTable = ...
+-- HeroLib
+local HL         = HeroLib
+local Cache      = HeroCache
+local Unit       = HL.Unit
+local Player     = Unit.Player
+local Target     = Unit.Target
+local Pet        = Unit.Pet
+local Spell      = HL.Spell
+local MultiSpell = HL.MultiSpell
+local Item       = HL.Item
+-- HeroRotation
+local HR         = HeroRotation
+
+
+local function OnInit ()
+  HR.Print("Survival Hunter rotation is currently a work in progress.")
+end
+
+HR.SetAPL(255, APL, OnInit)
