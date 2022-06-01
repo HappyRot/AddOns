94fa6f8f6b15cbc86f186ca41203bc78ddc4f3df
HappyRot <104349898+HappyRot@users.noreply.github.com>
2022-06-01 17:56:17 -0400

    Updated Hero DBC LIB files for 9.25
    updated hero dbc and lib files 9.25

--- HeroDBC/DBC/Meta.lua
+++ HeroDBC/DBC/Meta.lua
@@ -1,2 +1,2 @@
-HeroDBC.DBC.metaVersion = "9.2.0.42979"
-HeroDBC.DBC.metaTime = "2022-04-08T09:13:05.957288"
+HeroDBC.DBC.metaVersion = "9.2.5.43903"
+HeroDBC.DBC.metaTime = "2022-05-31T18:56:14.210243"
