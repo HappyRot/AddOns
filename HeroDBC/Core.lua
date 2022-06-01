68a8e00488bbb4bfa2768ddcf03b6f6f82c4fdae
HappyRot <104349898+HappyRot@users.noreply.github.com>
2022-06-01 18:00:31 -0400

    cleanup internal
    purge

--- HeroDBC/Core.lua
+++
@@ -1,8 +0,0 @@
---- ============================ HEADER ============================
---- ======= LOCALIZE =======
--- Addon
-local addonName, HDBC = ...
-
---- ======= GLOBALIZE =======
-HeroDBC = HDBC
-HDBC.DBC = {}
--- HeroDBC/DBC/Meta.lua
+++
@@ -1,2 +0,0 @@
-HeroDBC.DBC.metaVersion = "9.2.0.42979"
-HeroDBC.DBC.metaTime = "2022-04-08T09:13:05.957288"
