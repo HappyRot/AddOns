01d6d276688ec5d8d02c1abd06cfb5d8a3861a83
AyaProfiles <77168918+AyaProfiles@users.noreply.github.com>
2022-12-08 23:48:40 -0600

    Lib - dummy updates + buff tweaks

--- HeroLib/Class/Unit/Main.lua
+++ HeroLib/Class/Unit/Main.lua
@@ -235,6 +235,18 @@ local DummyUnits = {
   [174487] = true, -- Training Dummy
   [174488] = true, -- Raider's Training Dummy
   [174491] = true, -- Tanking Dummy
+  -- DargonFlight Valdrakken
+  [198594] = true, -- Cleave Training Dummy
+  [194648] = true, -- Training Dummy
+  [189632] = true, -- Animated Duelist
+  [194643] = true, -- Dungeoneer's Training Dummy
+  [194644] = true, -- Dungeoneer's Training Dummy
+  [197833] = true, -- PvP Training Dummy
+  [189617] = true, -- Boulderfist
+  [194649] = true, -- Normal Tank Dummy
+  -- DargonFlight Iskaara
+  [193563] = true, -- Training Dummy
+  [196394] = true, -- Tuskarr Training Dummy (toy)
   -- Other
   [65310] = true, -- Turnip Punching Bag
 }
