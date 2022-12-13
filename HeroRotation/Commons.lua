6e720da8c3d5f6b5363f13a4a9fd00a2165ebead
AyaProfiles <77168918+AyaProfiles@users.noreply.github.com>
2022-12-13 15:55:48 -0600

    Core - Support for new DPS pots

--- HeroRotation/Commons.lua
+++ HeroRotation/Commons.lua
@@ -1,20 +1,20 @@
 local e, t = ...
 local a = HeroLib
-local c, y = HeroCache, a.Utils
-local r = a.Unit
-local s = r.Player
-local e = r.Target
-local h = a.Spell
-local o = a.Item
-local n = pairs
-local w = string.gsub
+local c, w = HeroCache, a.Utils
+local h = a.Unit
+local s = h.Player
+local e = h.Target
+local r = a.Spell
+local n = a.Item
+local o = pairs
+local a = string.gsub
 local f = UnitInParty
 local m = UnitInRaid
 t.Commons = {  }
 local a = {  }
 t.Commons.Everyone = a
-local d = t.GUISettings.General
-local l = t.GUISettings.Abilities
+local l = t.GUISettings.General
+local d = t.GUISettings.Abilities
 function a.TargetIsValid()
     return e:Exists() and s:CanAttack(e) and not e:IsDeadOrGhost()
 end
@@ -27,8 +27,8 @@ function a.CanDoTUnit(e, t)
     return e:Health() >= t or e:IsDummy()
 end
 
-local u = { Buff = { h(191941), h(204151), h(239932) }, Debuff = { 348074, 358988, 347607, 351180, 353929 }, Cast = { 197810, 197418, 198079, 214003, 235751, 193668, 227493, 228852, 193211, 200732, 241635, 241636, 236494, 239932, 254919, 244899, 245458, 248499, 258039, 346985, 350828, 350202, 350475, 351066, 350422, 352538, 350732, 348071 }, Channel = {  } }
-local l = { Buff = {  }, Debuff = {  }, Cast = { 350732, 355352, 353603 }, Channel = {  } }
+local u = { Buff = { r(191941), r(204151), r(239932) }, Debuff = { 348074, 358988, 347607, 351180, 353929 }, Cast = { 197810, 197418, 198079, 214003, 235751, 193668, 227493, 228852, 193211, 200732, 241635, 241636, 236494, 239932, 254919, 244899, 245458, 248499, 258039, 346985, 350828, 350202, 350475, 351066, 350422, 352538, 350732, 348071 }, Channel = {  } }
+local d = { Buff = {  }, Debuff = {  }, Cast = { 350732, 355352, 353603 }, Channel = {  } }
 function a.ActiveMitigationNeeded()
     if not s:IsTanking(e) then
         return false
@@ -38,17 +38,17 @@ function a.ActiveMitigationNeeded()
         return true
     end
 
-    for a, t in n(u.Buff) do
+    for a, t in o(u.Buff) do
         if e:BuffUp(t, true) then
             return true
         end
 
     end
 
-    for e, t in n(u.Debuff) do
-        local a, a, a, a, a, a, a, a, a, e, a, a, a, a, a, a = UnitDebuff("player", t)
-        if e ~= nil then
-            if t == e then
+    for t, e in o(u.Debuff) do
+        local a, a, a, a, a, a, a, a, a, t, a, a, a, a, a, a = UnitDebuff("player", e)
+        if t ~= nil then
+            if e == t then
                 return true
             end
 
@@ -64,18 +64,18 @@ function a.BigActiveMitigationNeeded()
         return false
     end
 
-    if l.Cast[e:CastSpellID()] then
+    if d.Cast[e:CastSpellID()] then
         return true
     end
 
-    for a, t in n(l.Buff) do
+    for a, t in o(d.Buff) do
         if e:BuffUp(t, true) then
             return true
         end
 
     end
 
-    for e, t in n(l.Debuff) do
+    for e, t in o(d.Debuff) do
         local a, a, a, a, a, a, a, a, a, e, a, a, a, a, a, a = UnitDebuff("player", i)
         if e ~= nil then
             if t == e then
@@ -90,9 +90,9 @@ function a.BigActiveMitigationNeeded()
 end
 
 do
-    local e = { Debuff = { h(243961) } }
+    local e = { Debuff = { r(243961) } }
     function s:HealingAbsorbed()
-        for t, e in n(e.Debuff) do
+        for t, e in o(e.Debuff) do
             if s:DebuffUp(e, true) then
                 return true
             end
@@ -104,14 +104,14 @@ do
 
 end
 
-function a.Interrupt(n, o, i, a)
-    if d.InterruptEnabled and e:IsInterruptible() and e:IsInRange(n) then
+function a.Interrupt(i, o, n, a)
+    if l.InterruptEnabled and e:IsInterruptible() and e:IsInRange(i) then
                 if o:IsCastable() then
-            if t.Cast(o, i) then
+            if t.Cast(o, n) then
                 return "Cast " .. o:Name() .. " (Interrupt)"
             end
 
-        elseif d.InterruptWithStun and e:CanBeStunned() then
+        elseif l.InterruptWithStun and e:CanBeStunned() then
             if a then
                 for e = 1, #a do
                     if a[e][1]:IsCastable() and a[e][3]() then
@@ -132,19 +132,19 @@ function a.Interrupt(n, o, i, a)
 end
 
 function a.IsSoloMode()
-    return d.SoloMode and not s:IsInRaidArea() and not s:IsInDungeonArea()
+    return l.SoloMode and not s:IsInRaidArea() and not s:IsInDungeonArea()
 end
 
-function a.CastCycle(o, i, a, r, h, s)
-    if a(e) then
-        return t.Cast(o, h, s, r)
+function a.CastCycle(a, r, i, s, h, n)
+    if i(e) then
+        return t.Cast(a, h, n, s)
     end
 
     if t.AoEON() then
-        local s = e:GUID()
-        for i, e in n(i) do
-            if e:GUID() ~= s and not e:IsFacingBlacklisted() and not e:IsUserCycleBlacklisted() and a(e) then
-                t.CastLeftNameplate(e, o)
+        local n = e:GUID()
+        for o, e in o(r) do
+            if e:GUID() ~= n and not e:IsFacingBlacklisted() and not e:IsUserCycleBlacklisted() and i(e) then
+                t.CastLeftNameplate(e, a)
                 break
             end
 
@@ -154,26 +154,26 @@ function a.CastCycle(o, i, a, r, h, s)
 
 end
 
-function a.CastTargetIf(h, m, c, s, a, l, d, r)
-    local u = (not a or (a and a(e)))
-    if not t.AoEON() and u then
-        return t.Cast(h, d, r, l)
+function a.CastTargetIf(s, m, c, h, a, u, l, d)
+    local r = (not a or (a and a(e)))
+    if not t.AoEON() and r then
+        return t.Cast(s, l, d, u)
     end
 
     if t.AoEON() then
-        local o, i = nil, nil
-        for t, e in n(m) do
-            if not e:IsFacingBlacklisted() and not e:IsUserCycleBlacklisted() and (e:AffectingCombat() or e:IsDummy()) and (not i or y.CompareThis(c, s(e), i)) then
-                o, i = e, s(e)
+        local i, n = nil, nil
+        for t, e in o(m) do
+            if not e:IsFacingBlacklisted() and not e:IsUserCycleBlacklisted() and (e:AffectingCombat() or e:IsDummy()) and (not n or w.CompareThis(c, h(e), n)) then
+                i, n = e, h(e)
             end
 
         end
 
-        if o then
-                        if u and (o:GUID() == e:GUID() or i == s(e)) then
-                return t.Cast(h, d, r, l)
-            elseif ((a and a(o)) or not a) then
-                t.CastLeftNameplate(o, h)
+        if i then
+                        if r and (i:GUID() == e:GUID() or n == h(e)) then
+                return t.Cast(s, l, d, u)
+            elseif ((a and a(i)) or not a) then
+                t.CastLeftNameplate(i, s)
             end
 
         end
@@ -182,23 +182,23 @@ function a.CastTargetIf(h, m, c, s, a, l, d, r)
 
 end
 
-function a.GroupBuffMissing(a)
-    local t = 40
-    if a:Name() == "Battle Shout" then
-        t = 100
+function a.GroupBuffMissing(t)
+    local a = 40
+    if t:Name() == "Battle Shout" then
+        a = 100
     end
 
     local e
         if m("player") then
-        e = r.Raid
+        e = h.Raid
     elseif f("player") then
-        e = r.Party
+        e = h.Party
     else
         return false
     end
 
-    for o, e in n(e) do
-        if e:Exists() and e:IsInRange(t) and e:BuffDown(a, true) then
+    for o, e in o(e) do
+        if e:Exists() and e:IsInRange(a) and e:BuffDown(t, true) then
             return true
         end
 
@@ -235,48 +235,51 @@ function a.GetCurrentEmpowerData(n)
 end
 
 function a.PotionSelected()
-    local e = c.Persistent.Player.Class[1]
-    e = w(e, "%s+", "")
-    local a = c.Persistent.Player.Spec[2]
-    local e = t.GUISettings.APL[e][a].PotionType.Selected
-    local a = { 191914, 191913, 191912, 191907, 191906, 191905, 191383, 191382, 191381, 191389, 191388, 191387 }
-    local i = { 191365, 191364, 191363 }
-    local t = { 191368, 191367, 191366 }
-    local n = { 191401, 191400, 191399 }
+    local a = { "Warrior", "Paladin", "Hunter", "Rogue", "Priest", "DeathKnight", "Shaman", "Mage", "Warlock", "Monk", "Druid", "DemonHunter", "Evoker" }
+    local e = c.Persistent.Player.Class[3]
+    local o = a[e]
+    local a = { [250] = "Blood", [251] = "Frost", [252] = "Unholy", [577] = "Havoc", [581] = "Vengeance", [102] = "Balance", [103] = "Feral", [104] = "Guardian", [105] = "Restoration", [1467] = "Devastation", [1468] = "Preservation", [253] = "BeastMastery", [254] = "Marksmanship", [255] = "Survival", [62] = "Arcane", [63] = "Fire", [64] = "Frost", [268] = "Brewmaster", [269] = "Windwalker", [270] = "Mistweaver", [65] = "Holy", [66] = "Protection", [70] = "Retribution", [256] = "Discipline", [257] = "Holy", [258] = "Shadow", [259] = "Assassination", [260] = "Outlaw", [261] = "Subtlety", [262] = "Elemental", [263] = "Enhancement", [264] = "Restoration", [265] = "Affliction", [266] = "Demonology", [267] = "Destruction", [71] = "Arms", [72] = "Fury", [73] = "Protection" }
+    local e = c.Persistent.Player.Spec[1]
+    local e = a[e]
+    local e = t.GUISettings.APL[o][e].PotionType.Selected
+    local t = { 191914, 191913, 191912, 191907, 191906, 191905, 191383, 191382, 191381, 191389, 191388, 191387 }
+    local a = { 191365, 191364, 191363 }
+    local o = { 191368, 191367, 191366 }
+    local i = { 191401, 191400, 191399 }
                 if e == "Power" then
-        for t, e in ipairs(a) do
-            if o(e):IsUsable() then
-                return o(e)
+        for t, e in ipairs(t) do
+            if n(e):IsUsable() then
+                return n(e)
             end
 
         end
 
     elseif e == "Frozen Focus" then
-        for t, e in ipairs(i) do
-            if o(e):IsUsable() then
-                return o(e)
+        for t, e in ipairs(a) do
+            if n(e):IsUsable() then
+                return n(e)
             end
 
         end
 
     elseif e == "Chilled Clarity" then
-        for t, e in ipairs(t) do
-            if o(e):IsUsable() then
-                return o(e)
+        for t, e in ipairs(o) do
+            if n(e):IsUsable() then
+                return n(e)
             end
 
         end
 
     elseif e == "Shocking Disclosure" then
-        for t, e in ipairs(n) do
-            if o(e):IsUsable() then
-                return o(e)
+        for t, e in ipairs(i) do
+            if n(e):IsUsable() then
+                return n(e)
             end
 
         end
 
     else
-        return o(191389)
+        return nil
     end
 
 end
